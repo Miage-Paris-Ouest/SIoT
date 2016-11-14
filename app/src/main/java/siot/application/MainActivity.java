c0838b0d2e37c5aaa6ac92dead6b31a527f1c2b4
pascalorsini <pascal.orsini@outlook.com>
2016-11-14 23:14:25 +0100

    push

---
+++ app/src/main/java/siot/application/MainActivity.java
@@ -0,0 +1,26 @@
+package siot.application;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button suivant = (Button) findViewById(R.id.suivant);
+
+        suivant.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChoixCapteurs.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
